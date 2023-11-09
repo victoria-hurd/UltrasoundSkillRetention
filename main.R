09d803115ad20ed6acdf3c16f4d73d259a1776db
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2023-11-09 13:13:41 -0700

    Create main.R

---
+++ main.R
@@ -0,0 +1,16 @@
+# AUTHOR:       Victoria Hurd
+# DATE CREATED: 8/02/23
+# LAST EDITED:  11/1/23
+# PROJECT:      ButterflAI Ultrasound Study
+# TASK:         Data Analysis - Wrong Organ Analysis
+
+### ADMIN ###
+path='/Users/vickihurd/Documents/Research/Astrosonography/Skill Retention Study/Code'
+setwd(path)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(tidyverse)
+
+# ---------------------------------------------------------------
